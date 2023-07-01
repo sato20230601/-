be4fa987b35eb03547bfef3d76819bca42dd9b7f
sato20230601 <135215169+sato20230601@users.noreply.github.com>
2023-07-02 01:32:53 +0900

    Add files via upload

---
+++ DB_CRE_08_AsetHoldingData.py
@@ -0,0 +1,78 @@
+#import traceback
+#traceback.print_exc()
+
+import os
+import sys
+import logging
+from datetime import datetime
+from configparser import ConfigParser
+
+# 共通関数の読み込み
+import DB_Common_Utils
+
+# 関数用の定義ファイルをインクルード
+from DB_CRE_00_Utils import create_table, check_table_existence, read_create_statements, process_sql_files
+
+def main():
+
+    # ロギングの設定
+    # 一般的なログレベルの階層 CRITICAL > ERROR > WARNING > INFO > DEBUG
+
+    log_directory = r"C:\Users\sabe2\OneDrive\デスクトップ\Python\06_DATABASE\06-03_SRC\LOG"
+    os.makedirs(log_directory, exist_ok=True)
+    log_file = os.path.join(log_directory, f"log_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log")
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    # 処理開始
+    logger.info("処理を開始します。")
+
+    # SQLファイル処理
+    # デフォルトのファイルパスを設定します
+    default_file_path = r"C:\Users\sabe2\OneDrive\デスクトップ\Python\06_DATABASE\06-02_OBJ\01-8_CRE_AsetHoldingData.txt"
+
+    # コマンドライン引数からファイルパスとオプションを取得します
+    if len(sys.argv) < 2:
+        file_path = default_file_path  # デフォルトのファイルパスを使用します
+    else:
+        file_path = sys.argv[1]
+
+    try:
+        # MySQLに接続
+        cnx = DB_Common_Utils.get_mysql_connection()
+
+        # カーソルを取得
+        cursor = cnx.cursor()
+
+        table_name = process_sql_files(cursor,file_path, logger)
+
+        # トランザクションをコミットする
+        cnx.commit()  
+
+    except Exception as e:
+        logger.exception("予期しないエラーが発生しました。")
+
+    finally:
+        # カーソルと接続を閉じる
+        if cursor:
+            cursor.close()
+        if cnx:
+            cnx.close()
+
+        logger.info("カーソルと接続を閉じました。")
+
+if __name__ == "__main__":
+    main()
